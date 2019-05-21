8d12e2cc5c8134d342bf9f6b71dc67372bc96d9d
Rosa Kang <rosakang@Ritas-MBP.localdomain>
2019-05-21 16:54:11 -0700

    nav and header css done

--- client/in-stock/src/App.js
+++ client/in-stock/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Nav from "../src/component/Nav";
 import InvHeader from "../src/component/InvHeader";
-import LocHeader from "../src/component/LocHeader";
+// import LocHeader from "../src/component/LocHeader";
+import "../src/styles/styles.css";
 import "./App.css";
 
 class App extends React.Component {
@@ -10,7 +11,6 @@ class App extends React.Component {
       <>
         <Nav />
         <InvHeader />
-        <LocHeader />
       </>
     );
   }
