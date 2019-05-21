a38c36f4c62a309e2f69e73ab579d83f3108975b
Luke Keller <39537709+Short-Hop@users.noreply.github.com>
2019-05-21 14:49:37 -0700

    Created and styled the Inventory component

--- client/in-stock/src/App.js
+++ client/in-stock/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './styles/styles.css';
+import Inventory from "./components/Inventory"
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        
+        <Inventory></Inventory>
       </div>
     );
   }
