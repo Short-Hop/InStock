f60a8103a899a299af679ebf68048972828a3219
Marion Cointre <marion.cointre@gmail.com>
2019-05-23 09:45:07 -0700

    App-architecture
    Ticket https://brainstationeducation.atlassian.net/browse/VS-69

--- client/in-stock/src/App.js
+++ client/in-stock/src/App.js
@@ -24,7 +24,7 @@ class App extends React.Component {
         {/* <LocHeader /> */}
         <CreateProduct />
         <CreateWarehouse />
-        <Location />
+        {/* <Location /> */}
       </>
     );
   }
--- client/in-stock/src/components/Switch.js
+++ client/in-stock/src/components/Switch.js
@@ -1,23 +1,29 @@
-import React, { Component } from "react";
-import Switch from "react-switch";
+// import React, { Component } from "react";
+// import Switch from "react-switch";
 
-class Switch extends Component {
-  constructor() {
-    super();
-    this.state = { checked: false };
-    this.handleChange = this.handleChange.bind(this);
-  }
+// class Switch extends Component {
+//   constructor() {
+//     super();
+//     this.state = { checked: false };
+//     this.handleChange = this.handleChange.bind(this);
+//   }
 
-  handleChange(checked) {
-    this.setState({ checked });
-  }
+//   handleChange(checked) {
+//     this.setState({ checked });
+//   }
 
-  render() {
-    return (
-      <label>
-        <Switch onChange={this.handleChange} checked={this.state.checked} />
-      </label>
-    );
-  }
-}
-export default Switch;
+//   render() {
+//     return (
+//       <label>
+//         <Switch
+//           uncheckedIcon={false}
+//           checkedIcon={false}
+//           onChange={this.handleChange}
+//           checked={this.state.checked}
+//           onColor={"#00B200"}
+//         />
+//       </label>
+//     );
+//   }
+// }
+// export default Switch;
