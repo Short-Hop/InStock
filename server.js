db67dc9ca453de3dbd9c63cadaf08fb60d290e18
Parva9eh <berespectful9@gmail.com>
2019-05-23 23:35:33 -0700

    Redesigned the BackEnd and added GET endpoints

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/warehouses", require("./routes/api/warehouses"));
+
+// listening
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
