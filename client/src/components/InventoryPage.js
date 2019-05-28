bdcb616b5fff8be676161f39a72b2238c5417dc0
Luke Keller <39537709+Short-Hop@users.noreply.github.com>
2019-05-27 18:03:58 -0700

    Fixed inventorypage error

--- client/src/components/InventoryPage.js
+++ client/src/components/InventoryPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import Nav from "./Nav";
 import Header from "./Header";
@@ -24,28 +23,3 @@ class InventoryPage extends React.Component {
 }
 
 export default InventoryPage;
-=======
-import React from 'react';
-import Nav from "./Nav"
-import Header from "./Header";
-import Inventory from "./Inventory";
-import CreateProduct from './CreateProduct';
-import axios from "axios";
-
-class InventoryPage extends React.Component {
-    
-    render() {
-        return (
-            <>
-                <Nav page={this.props.match.path}/>
-                <Header title="Inventory" />
-                <Inventory warehouseArray={this.props.warehouseArray}/>
-                <CreateProduct/>
-            </>
-        )
-
-    }
-}
-
-export default InventoryPage;
->>>>>>> parent of adefd7c8d... Merge pull request #36 from Short-Hop/POST-new-warehouse
