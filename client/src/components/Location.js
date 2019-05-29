97d428c9ca55043e6e5fe5d111a79539e74ea723
Parva9eh <berespectful9@gmail.com>
2019-05-28 23:00:33 -0700

    Improved the style of the Location List Component

--- client/src/components/Location.js
+++ client/src/components/Location.js
@@ -1,18 +1,15 @@
 import React from "react";
 import rightArrow from "../assets/Icons/SVG/Icon-arrow-right.svg";
-import WarehouseList from "./WarehouseList"
 
 export default class Location extends React.Component {
   render() {
-
     console.log(this.props.warehouseArray);
     return (
       <>
-        {/* <WarehouseList warehouses={this.state.warehouses} />; */}
         <section className="location">
           <table className="location__table">
             <tbody>
-              {/* <MainRow /> */}
+              <MainRow />
 
               {this.props.warehouseArray.map(warehouse => (
                 <LocationEntry warehouse={warehouse} />
@@ -41,6 +38,10 @@ export default class Location extends React.Component {
             <th>
               <h5>CATEGORIES</h5>
             </th>
+            <th />
+          </tr>
+          <tr>
+            <br />
           </tr>
         </>
       );
@@ -51,10 +52,9 @@ export default class Location extends React.Component {
         <>
           <tr className="row1">
             <td className="row1__col-mobile">
-              <h5 className="main-col">WAREHOUSE</h5>
               <div className="arrow-control">
                 <h3 className="title">
-                  {props.warehouse.name} {props.warehouse.number}
+                  {props.warehouse.name} {props.warehouse.number + 1}
                 </h3>
                 <img className="rightArrowImg1" src={rightArrow} />
               </div>
@@ -66,9 +66,8 @@ export default class Location extends React.Component {
             </td>
           </tr>
           <tr className="row2">
-            <td className="row2__col-desk">
+            <td className="row2__col-desk row2__item">
               <div className="arrow-control">
-                <h5 className="main-col">WAREHOUSE</h5>
                 <h3 className="title">
                   {props.warehouse.name} {props.warehouse.number + 1}
                 </h3>
@@ -81,32 +80,28 @@ export default class Location extends React.Component {
                 </h4>
               </div>
             </td>
-            <td>
-              <h5 className="main-col">CONTACT</h5>
-              <h4>{props.warehouse.contact.position}</h4>
+            <td className="row2__item">
               <h4>{props.warehouse.contact.name}</h4>
+              <h4 className="position">{props.warehouse.contact.position}</h4>
             </td>
-            <td>
-              <h5 className="main-col">CONTACT INFORMATION</h5>
+            <td className="row2__item">
               <h4>{props.warehouse.contact.phone}</h4>
               <h4>{props.warehouse.contact.email}</h4>
             </td>
-            <td>
-              <h5 className="main-col">CATEGORIES</h5>
+            <td className="row2__item">
               <h4>
                 {props.warehouse.categories[0]}, {props.warehouse.categories[1]}
                 , {props.warehouse.categories[2]},{" "}
-                {props.warehouse.categories[3]},
               </h4>
               <h4>
-                {props.warehouse.categories[4]}, {props.warehouse.categories[5]}
+                {props.warehouse.categories[3]}, {props.warehouse.categories[4]}
               </h4>
             </td>
-            <td className="row2__col-last">
+            <td className="row2__col-last row2__item">
               <img className="rightArrowImg2" src={rightArrow} />
             </td>
           </tr>
-          <hr />
+          <hr className="hr" />
         </>
       );
     }
